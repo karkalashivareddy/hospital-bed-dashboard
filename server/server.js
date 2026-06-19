1bd69e2aa2f1df6812d9258e8685320831e6f96c
K. Shiva Reddy <yourgmail@gmail.com>
2026-06-19 21:47:23 +0530

    fix: restore env configuration and backend startup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const bedsRouter = require('./routes/beds');
-
-dotenv.config();
-
+const bedsRouter = require('./routes/bedRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
