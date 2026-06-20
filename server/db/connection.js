c530b617ea9868ece01c5e73b8fe7ba39af25265
K. Shiva Reddy <yourgmail@gmail.com>
2026-06-20 09:14:40 +0530

    updated hospital bed dashboard

--- server/db/connection.js
+++ server/db/connection.js
@@ -12,3 +12,8 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
+console.log("DB_NAME =", process.env.DB_NAME);
+require('dotenv').config();
